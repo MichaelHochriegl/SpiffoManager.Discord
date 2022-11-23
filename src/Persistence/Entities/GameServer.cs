0f1481ec0ce766b385d17c80c9c2d6bc457ec378
MichaelHochriegl <michael@hochriegl.dev>
2022-11-23 21:55:23 +0100

    add doc comments

--- src/Persistence/Entities/GameServer.cs
+++ src/Persistence/Entities/GameServer.cs
@@ -1,9 +1,23 @@
 namespace Persistence.Entities;
 
+/// <summary>
+/// Entity that represents a gameserver
+/// </summary>
 public class GameServer
 {
+    /// <summary>
+    /// Unique ID of the entity
+    /// </summary>
     public ulong Id { get; set; }
+    
+    /// <summary>
+    /// Name of the server with which it can be accessed
+    /// </summary>
     public string ServerName { get; set; } = "pzserver";
+    
+    /// <summary>
+    /// Path where the actual LGSM instance lies
+    /// </summary>
     public string ServerInstallPath { get; set; } = "../";
 
     private GameServer()
