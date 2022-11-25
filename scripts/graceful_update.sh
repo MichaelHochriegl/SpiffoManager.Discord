79cb72d985ef1b8ae8d398b977e7fd45cd9566c9
MichaelHochriegl <michael@hochriegl.dev>
2022-11-25 21:28:49 +0100

    add scripts

---
+++ scripts/graceful_update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Save this script as "graceful.sh"
+# Make it executable with "chmod +x graceful.sh"
+# Then run it when you want to notify players 15 minutes before the
+# server restarts with:
+#   ./graceful.sh
+session=$1
+tmux send-keys -t $session "servermsg \"Check auf neue Version...\"" Enter
+sleep 5
+~/pzserver update
+
